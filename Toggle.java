c7fef54b6b0054aaff544e418a026e45a8b246f1
Whitewolf1237 <132841363+Whitewolf1237@users.noreply.github.com>
2024-04-13 20:24:11 +0530

    set bit

--- Toggle.java
+++ Toggle.java
@@ -1,3 +1,13 @@
 public class Toggle {
-    
+    public static void main(String[] args) {
+        int n = 36;
+        int i = 5;
+        int cmask = 1<<i;
+        if((n & cmask)==0){
+            System.out.println("OFF");
+        }
+        else{
+            System.out.println("OFF");
+        }
+    }
 }
---
+++ rightMostSetBit.java
@@ -0,0 +1,24 @@
+public class rightMostSetBit {
+    static int posOfRMSPEasy(int n){
+        // int m = n & (n-1);
+        // n = n^m;
+        return (int)(Math.log10(n^(n & (n-1))) / Math.log10(2)) +1;
+    }
+
+    static int posOfRMSP(int n){
+        int m=1;
+        int pos=0;
+        if(n == 0){
+            return -1;
+        }
+        while( (n & m) == 0){
+            m = m<<1;
+            pos++;
+        }
+        return pos+1; //zero based indexing so +1
+    }
+    public static void main(String[] args) {
+        System.out.println(posOfRMSPEasy(40));
+        System.out.println(posOfRMSP(40));
+    }
+}
