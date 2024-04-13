fb0320c7ad82c1020c10e18a34ff4b6033331b21
Whitewolf1237 <132841363+Whitewolf1237@users.noreply.github.com>
2024-04-13 19:41:29 +0530

    Bit manipulation

---
+++ bitManipulationTwo.java
@@ -0,0 +1,6 @@
+public class bitManipulationTwo {
+    public static void main(String[] args) {
+        System.out.println(5>>1);  //divide by two
+        System.out.println(5<<1);  //multiply by two
+    }
+}
