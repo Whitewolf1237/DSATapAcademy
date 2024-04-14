1a7fb36da515b88af1836f2bb2ff95972e4b4449
Whitewolf1237 <132841363+Whitewolf1237@users.noreply.github.com>
2024-04-14 15:13:35 +0530

    trailing zeros in a binary number

---
+++ NumberOfTrailingZeroBit.java
@@ -0,0 +1,9 @@
+public class NumberOfTrailingZeroBit {
+    static int numberOfTz(int n){
+        return (int)(Math.log10(n^(n & (n-1))) / Math.log10(2)) ;
+    }
+    public static void main(String[] args) {
+        System.out.println(numberOfTz(128));
+    }
+    
+}
