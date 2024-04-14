9a57c7aa6c6656747b7f63d4fc5294ee9380a1fa
Whitewolf1237 <132841363+Whitewolf1237@users.noreply.github.com>
2024-04-14 14:54:52 +0530

    bit

--- ConsecutiveOnes.java
+++ ConsecutiveOnes.java
@@ -1,3 +1,14 @@
 public class ConsecutiveOnes {
-    
+    static int consecutiveOnes(int n){
+        int count = 0;
+
+        while(n>0){
+            n=(n&(n<<1));
+            count++;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        System.out.println(consecutiveOnes(101));
+    }
 }
---
+++ SwapBits.java
@@ -0,0 +1,11 @@
+public class SwapBits {
+    static int swapEvenOddBits(int n){
+        return ((n&0xaaaaaaaa)>>>1)|((n&0x55555555)<<1); //left paranthesis moves even bit to odd places and
+                                                        //right paranthesis moves odd bits to even places
+    }
+
+    public static void main(String[] args) {
+        System.out.println(swapEvenOddBits(181));
+    }
+    
+}
